3bdb4d2b30423e3ad393e18b4ba51e778ea6e8bd
Chihaoui Dhoha <chihaoui.dho7a@gmail.com>
2019-10-08 09:31:49 +0100

    add 2

---
+++ components/footer/Footer.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="footer">
+   
+       <nav>
+        <ul>
+          <li>
+            <a href="https://sqoin.us/coin/">
+              Sqoin
+            </a>
+          </li>
+        
+          <li>
+            <a href="https:///blog.creative-tim.com">
+              SQoin Galerie
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright">
+        &copy; {{ year }}, Created and Coded by
+        
+        <a href="https://sqoin.us/coin/" target="_blank" rel="noopener"
+          >Sqoin TEAM</a
+        >.
+      </div></div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+
+  props: {
+    backgroundColor: String,
+    type: String
+  },
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+     
+ .copyright{float:right;}
+ .footer{
+    height:50px;
+    background: black;
+    color: white;
+    padding: 24px 0;
+
+    
+
+    nav{
+        display: inline-block;
+        float: left;
+    }
+
+    ul{
+        margin-bottom: 0;
+        padding: 0;
+        list-style: none;
+
+        li{
+            display: inline-block;
+
+            a{
+                color: orange;
+                
+                text-transform: uppercase;
+                text-decoration: none;
+
+                &:hover{
+                    text-decoration: none;
+                }
+            }
+        }
+    }
+
+    
+
+    &:after{
+        display: table;
+        clear: both;
+        content: " ";
+    }
+}
+
+ 
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+         Welcome in your E-COMMERCE Sqoin <i class="fa fa-heart heart"></i>
+        </h1>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background-image:url('../../static/back.png');
+     height:250px;
+     
+    }
+    h1, h2 {
+      color: black;
+  text-align:center;
+    }
+    
+</style>
